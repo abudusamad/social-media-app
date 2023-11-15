7120f247d2b6e254f622b4c8110461aefc24fbfc
abudusamad <abudusamed@gmail.com>
2023-11-15 23:25:15 +0000

    making prisma cleint for handle the backend of the database with Mongodb

---
+++ components/Form.tsx
@@ -0,0 +1,19 @@
+
+interface FormProps {
+    placeholder: string;
+    isComment?: boolean;
+    postId?: string;
+}
+
+const Form: React.FC<FormProps> = ({
+    placeholder,
+    isComment,
+    postId,
+
+}) => {
+    return (<div>
+        
+    </div> );
+}
+ 
+export default Form;

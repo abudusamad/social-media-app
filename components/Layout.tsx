3275e6d1a7c178a1e765a7bf58dcfaf61ec500f9
abudusamad <abudusamed@gmail.com>
2023-11-15 22:32:48 +0000

    Making the the follow bar of the app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,24 @@
+import Home from "@/app/page";
 import Sidebar from "./layout.tsx/Sidebar";
+import FollowBar from "./layout.tsx/follow-bar";
 
-
-interface LayoutProps {
-	children: React.ReactNode;
-}
 const Layout = () => {
 	return (
 		<div className=" h-screen bg-black">
 			<div className="container h-full mx-auto xl:px-32 max-w-6xl">
 				<div className=" grid grid-cols-4 h-full">
-                    <Sidebar/>
+					<Sidebar />
+					<div
+						className="
+					col-span-3
+					lg:col-span-2
+					border-x-[1px]
+					border-neutral-800
+					"
+					>
+						<Home />
+					</div>
+					<FollowBar />
 				</div>
 			</div>
 		</div>
---
+++ components/layout.tsx/follow-bar.tsx
@@ -0,0 +1,7 @@
+const FollowBar = () => {
+    return (<div className="text-white">
+        Flow Bar
+    </div> );
+}
+ 
+export default FollowBar;

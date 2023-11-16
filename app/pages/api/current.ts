7266a98da259f7355068696161647069e74722bb
abudusamad <abudusamed@gmail.com>
2023-11-16 21:54:09 +0000

    making  the session for the current user login

---
+++ app/pages/api/current.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "GET") {
+        return res.status(405).json({ message: "Method not allowed" });
+    }
+    
+    
+}
---
+++ libs/serverAuth.ts
@@ -0,0 +1,27 @@
+import { authOptions } from "@/app/pages/api/auth/[...nextauth[";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import prisma from "@/libs/prismadb";
+
+
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerSession(req, res, authOptions);
+
+    if (!session?.user?.email) {
+        throw new Error("Not signed in");
+
+    }
+
+    const currentUser = await prisma.user.findUnique({
+        where: {
+            email: session.user.email
+        }
+    })
+    if(!currentUser){
+        throw new Error("Not Signed In")
+    }
+    return {currentUser}
+}
+
+
+export default serverAuth;

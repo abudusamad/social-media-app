d30d4396fef8bcfce0c592322ab35cc79fe749d8
abudusamad <abudusamed@gmail.com>
2023-11-19 15:19:11 +0000

    Working on the clerk authentication

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
 		<div>
 			<Header label="Home" />
+			<UserButton afterSignOutUrl="/"/>
 		</div>
 	);
 }

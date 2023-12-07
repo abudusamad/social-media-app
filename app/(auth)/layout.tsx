a7022adab282b4f4090fa5d56d1f5a5642db1cea
abudusamad <abudusamed@gmail.com>
2023-12-07 23:10:21 +0000

    Cleaning up the code

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<div className=" h-full flex items-center justify-center">{children}</div>
-	);
-};
-
-export default AuthLayout;

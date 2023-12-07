1a81611a1bf39226af91aceb2b9e0f80f750c4e5
abudusamad <abudusamed@gmail.com>
2023-12-07 23:20:07 +0000

    making some changes to the files

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,8 @@
 import Header from "@/components/Header";
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
 	return (
 		<div>
 			<Header label="Home" />
-			<UserButton afterSignOutUrl="/"/>
 		</div>
 	);
 }
--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/libs/prismadb"
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
 	const session = await getServerSession(req, res, authOptions);
